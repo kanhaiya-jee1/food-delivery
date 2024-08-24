ccda64ed08583d07013f33a993bb7be22e4abc78
kanhaiya-jee1 <141860110+kanhaiya-jee1@users.noreply.github.com>
2024-08-24 11:56:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:4000",
+      "/api": "food-delivery-api-fawn.vercel.app",
     },
   },
   plugins: [react()],
